1f0a4a557a79ddecff445b819d8d4d46e2e90f2d
patch-e <p.crager@gmail.com>
2014-08-29 12:21:44 -0400

    Added routes

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,26 @@
+/*
+app.js
+Main app module for the angular BeersApp
+
+Copyright (c) 2014
+
+Patrick Crager
+
+*/
+
 angular.module('BeersApp', [
 
   'BeersApp.controllers',
-  'BeersApp.services'
+  'BeersApp.services',
+  'ngRoute'
 
-]);
+]).
+config(['$routeProvider', function($routeProvider) {
+
+  $routeProvider.
+  when("/", {templateUrl: "partials/table.html", controller: "beersController"}).
+  when("/list", {templateUrl: "partials/list.html", controller: "beersController"}).
+  when("/table", {templateUrl: "partials/table.html", controller: "beersController"}).
+  otherwise({redirectTo: '/'});
+
+}]);
