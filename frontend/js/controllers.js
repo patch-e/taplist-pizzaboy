b9d5295f8fc2d10fd100f8995514c300f195d4ef
patch-e <p.crager@gmail.com>
2014-08-29 05:55:36 -0400

    Code cleanup and loading indication handling

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -1,9 +1,13 @@
 angular.module('BeersApp.controllers', []).
 controller('beersController', function($scope, beerAPIservice) {
-    $scope.nameFilter = null;
-    $scope.beersList = [];
 
-    beerAPIservice.getBeers().success(function (response) {
-        $scope.beersList = response;
-    });
+  $scope.searchFilter = null;
+  $scope.beersList = [];
+
+  beerAPIservice.getBeers().success(function (response) {
+    document.getElementById('intro').style.display = 'none';
+    document.body.className = '';
+    $scope.beersList = response;
+  });
+
 });
