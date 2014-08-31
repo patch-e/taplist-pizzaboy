29d7dce2bb0e500026dc068ba6ccf2730693bcde
patch-e <p.crager@gmail.com>
2014-08-31 00:08:11 -0400

    Table view sorting

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -11,15 +11,44 @@ Patrick Crager
 angular.module('BeersApp.controllers', []).
 controller('beersController', function($scope, beerAPIservice) {
 
+  // list of table headings for table view
+  $scope.headings = [
+    {display: '#',       column: 'number'},
+    {display: 'Name',    column: 'name'},
+    {display: 'Brewery', column: 'brewery'},
+    {display: 'Style',   column: 'style'},
+    {display: 'ABV',     column: 'abv'}
+  ];
+
+  // variable to hold current filter input
   $scope.searchFilter = null;
+
+  // array that is populated after API call finishes
   $scope.beersList = [];
 
-  beerAPIservice.getBeers().success(function (response) {
+  // sorting functionality
+  $scope.sort = {
+    column: 'number',
+    descending: false
+  };
+  $scope.changeSorting = function(column) {
+      var sort = $scope.sort;
+      if (sort.column == column) {
+        sort.descending = !sort.descending;
+      } else {
+        sort.column = column;
+        sort.descending = false;
+      }
+  };  
+
+  // fetch the beers through the getBeers() service call
+  beerAPIservice.getBeers().success(function(response) {
     $scope.beersList = response;
     
-    // remove the loading indication
+    // remove the background color, loading indication, and show the main content
+    document.body.className = '';
     document.getElementById('loading').style.display = 'none';
-    document.body.className = '';    
+    document.getElementById('main').style.display = 'block';
   });
 
 });
