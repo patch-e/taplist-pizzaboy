bb9c799d4856b7de6dfbbb5bd447804a810dec1b
patch-e <p.crager@gmail.com>
2014-09-06 07:47:59 -0400

    Navigation UI updates

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -25,3 +25,8 @@ config(['$routeProvider', function($routeProvider) {
   otherwise({redirectTo: '/'});
 
 }]);
+
+// misc non-angular event handlers
+$('.collapse .btn').click(function() {
+    $('.collapse.in').collapse('hide');
+});
