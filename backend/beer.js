0d060bf9df0a20a4f4448b6382ae19918018d5f1
patch-e <p.crager@gmail.com>
2014-12-02 23:56:56 -0500

    Updated for the new Al's / Pizza Boy site

--- backend/beer.js
+++ backend/beer.js
@@ -15,78 +15,64 @@ var server = http.createServer(function (req, res) {
 	var request = require('request'),
 			cheerio = require('cheerio'),
 			console = require('clim')(),
-			utils = require('./utils'),
+			utils   = require('./utils'),
 			results = [],
 			options = {
-				url: 'http://alsofhampden.com/verify.php',
-				jar: true,
-				json: true,
-				qs: {'over21': '1', 'redirect': 'beerlist.php'}
+				url: 'http://www.pizzaboybrewing.com/on-tap/',
+				json: true
 			};
 
 	request(options, function (error, response, html) {
 		if (!error && response.statusCode === 200) {
 			var $ = cheerio.load(html);
 
-			// loop over the hv_gridtables that represent beer sets
-			$('table.hv_gridtable').each(function(index, beerTable) {
+			var titles = $('#beer-menu-list h2');
+
+			// loop over the tables that represent beer lists
+			$('#beer-menu-list table').each(function(tableIndex, beerTable) {
+
 				var $beerTable = $(beerTable),
 						// result object to be added to the results array
 						result = {},
 						// get the title for this beerTable
-						title = $beerTable.find('.hv_gridheader').html().trim(),
-						// get the last updated timestamp for this beerTable
-						lastUpdated = $beerTable.next().text(),
-						// array to add beers in this set to
+						title = $(titles[tableIndex]).text(),
+						// array to add beers in this table to
 						beers = [];
 
-				// loop over the beers in this set
-				$beerTable.find('.hv_gridbeercell').each(function(index, beerCell) {
-					var $beerCell = $(beerCell),
-							beer;
+				// loop over the beer rows in this table
+				$beerTable.find('tbody > tr').each(function(rowIndex, beerRow) {
+					var $beerRow = $(beerRow);
 
-					// get the current beer's data
-					if ($beerCell.find('.hv_gridbeername').length > 0) {
-						var name = $beerCell.find('.hv_gridbeername').text(),
-								addl = $beerCell.find('.hv_gridbeeraddl').text(),
-								brewery = $beerCell.find('.hv_gridbreweryname').text(),
-								styles = $beerCell.find('.hv_gridbeerstyle'),
-								style = '',
-								abv = '';
-						// there are either one or two "style" values associated to a beer
-						// 1. the actual style
-						// 2. the ABV
-						// If we have two, assume the second is ABV
-						if (styles.length > 1) {
-							style = $beerCell.find('.hv_gridbeerstyle').eq(0).text();
-							abv = $beerCell.find('.hv_gridbeerstyle').eq(1).text();
-						} else {
-							style = $beerCell.find('.hv_gridbeerstyle').eq(0).text();
-						}
+					var beer = {
+						number: rowIndex + 1,
+						name: '',
+						addl: '',
+						brewery: '',
+						style: '',
+						abv: 0,
+						growler: false
+					};
 
-						// create a beer object from the parsed data
-						beer = {
-							number: index + 1,
-							name: utils.trim(name),
-							addl: utils.trim(addl),
-							brewery: utils.trim(brewery),
-							style: utils.trim(style),
-							abv: utils.parseABV(utils.trim(abv)),
-							kicked: false
-						};
-					} else {
-						// create a "kicked" beer object for this kicked beer
-						beer = utils.getKickedBeer(index + 1);
-					}
+					var cellCount = $beerRow.find('td').length,
+							// hack to handle varying cell amounts for tap list vs. all others
+							cellIndexModifier = (cellCount === 4 ? 1 : 0);
+
+					// loop over the beer cells in this row
+					$beerRow.find('td').each(function(cellIndex, beerCell) {
+						var $beerCell = $(beerCell),
+								cellIndex = $beerCell.index() + cellIndexModifier;
+								cellValue = utils.trim($beerCell.text());
+
+						// populates the passed in beer object by mapping cell text to beer properties
+						utils.mapBeerValues(cellIndex, cellValue, beer);
+					});
 
 					// add beer object to beers array
-					beers.push(beer);					
+					beers.push(beer);
 				});
 
 				// set the title for this beerTable
-				result.title = utils.parseTitle(title);
-				// set the last updated timestamp for this beerTable
-				result.lastUpdated = utils.parseLastUpdated(lastUpdated);
+				result.title = utils.trim(title);
 				// set the resulting beers array
 				result.beers = beers;
 				// push the finalized result in the overall results array
@@ -107,7 +93,7 @@ var server = http.createServer(function (req, res) {
 	});
 
 	// log usage
-	console.log(req.headers['x-forwarded-for'] + '\n' + req.headers['user-agent']);
+	console.log(req.headers['x-forwarded-for'] + '\n' + req.headers['user-agent'] + '\n\n');
 });
 
-server.listen(process.env.PORT);  
+server.listen(process.env.PORT);
--- backend/utils.js
+++ backend/utils.js
@@ -1,6 +1,6 @@
 /*
 utils.js
-Module that provides utility functions to beer.js
+Module that provides utility functions to mccrager nodejs apps
 
 Copyright (c) 2014
 
@@ -9,32 +9,21 @@ Patrick Crager
 */
 
 module.exports = {
+	addlRegEx: /\(([^()]+)\)$/,
+
 	// type safe trim function
-	// returns a trimmed string
+	// returns a trimmed string with continous spaces replaced with a single space
 	trim: function(s) {
 		// only invoke trim() if a string was passed in
 		if (typeof s === 'string') { 
-			s = s.trim().replace('\n', '').replace('\t', '');
+			s = s.trim().replace('\n', '').replace('\t', '').replace(/\s+/g,' ');
 		}
 		return s;
 	},
 
-	// returns a "kicked" beer object
-	getKickedBeer: function(i) {
-		return {
-			number: i,
-			name: 'KICKED!',
-			addl: null,
-			brewery: null,
-			style: null,
-			abv: null,
-			kicked: true
-		};
-	},
-
 	// parses the "ABV" text to return a floating point value
 	parseABV: function(s) {
-		if ( (typeof s === 'string') && (s.lastIndexOf('ABV:', 0) === 0) ) { 
+		if ( typeof s === 'string' ) { 
 			s = s.replace('ABV:', '').replace(' ', '').replace('%', '');
 		}
 		return parseFloat(s);
@@ -54,5 +43,41 @@ module.exports = {
 			s = s.replace('Beers', '').replace(' ', '');
 		}
 		return s;
+	},
+
+	// parses the name text to extract "additional" text in parens
+	parseAdditional: function(s) {
+		s = s.match(this.addlRegEx) || [''];
+		return s[0].replace('(', '').replace(')', '');
+	},
+
+	// parses the name text to remove "additional" text in parens
+	parseName: function(s) {
+		return s.replace(this.addlRegEx, '');
+	},
+
+	// maps values to properties of the beer object depending on index
+	mapBeerValues: function(index, value, beer) {
+		switch(index) {
+			case 0:
+				beer.number = parseInt(value, 10);
+				break;
+			case 1:
+				beer.brewery = value;
+				break;
+			case 2:
+				beer.name = this.parseName(value);
+				beer.addl = this.parseAdditional(value);
+				break;
+			case 3:
+				beer.style = value;
+				break;
+			case 4:
+				beer.abv = this.parseABV(value);
+				break;
+			case 5:
+				beer.growlerable = (value === 'Yes' ? true : false);
+				break;
+		}
 	}
 };
