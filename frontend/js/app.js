441a3a17b68bdb485ede819b9e3178b8f8ab3e0a
patch-e <p.crager@gmail.com>
2015-02-04 23:31:54 -0500

    Router fix

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -18,10 +18,9 @@ angular.module('BeersApp', [
 config(['$routeProvider', function($routeProvider) {
 
   $routeProvider.
-  when('/', {templateUrl: 'partials/table.html', controller: 'beersController'}).
-  when('/list', {templateUrl: 'partials/list.html', controller: 'beersController'}).
   when('/table', {templateUrl: 'partials/table.html', controller: 'beersController'}).
-  when('/block', {templateUrl: 'partials/block.html', controller: 'beersController'}).
-  otherwise({redirectTo: '/'});
+  when('/block', {templateUrl: 'partials/block.html', controller: 'beersController'}).  
+  when('/list', {templateUrl: 'partials/list.html', controller: 'beersController'}).
+  otherwise({redirectTo: '/table'});
 
 }]);
