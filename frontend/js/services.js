e3d7ba4dc4a556cbbf175700fdd459efaed9da3d
patch-e <p.crager@gmail.com>
2015-03-01 20:46:31 -0500

    Refactored services module

--- frontend/js/services.js
+++ frontend/js/services.js
@@ -11,26 +11,28 @@ Patrick Crager
 angular.module('BeersApp.services', []).
 factory('$beerAPIservice', function($http) {
 
-  var beerAPI = {};
+  var beerAPI = {
 
-  // list provides the JSON beer list
-  beerAPI.list = function() {
-    return $http({
-      method: 'GET',
-      url: '/nodejs/beer'
-    });
-  };
+    // list provides the JSON beer list
+    list: function() {
+      return $http({
+        method: 'GET',
+        url: '/nodejs/beer'
+      });
+    },
 
-  // search provides the JSON beer search result
-  beerAPI.search = function(brewery, name) {
-    return $http({
-      method: 'GET',
-      url: '/nodejs/beer/search',
-      params: {
-      	brewery: brewery,
-      	name: name
-      }
-    });
+    // search provides the JSON beer search result
+    search: function(brewery, name) {
+      return $http({
+        method: 'GET',
+        url: '/nodejs/beer/search',
+        params: {
+          brewery: brewery,
+          name: name
+        }
+      });
+    }
+
   };
 
   return beerAPI;
