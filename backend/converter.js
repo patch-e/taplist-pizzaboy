ac889be88a8b50ef93e874f14f8ae3064d5dff15
patch-e <p.crager@gmail.com>
2015-08-31 21:16:08 -0400

    new conversion

--- backend/converter.js
+++ backend/converter.js
@@ -32,6 +32,7 @@ module.exports = {
 			replace('2015', '').
 			replace('2016', '').
 			replace('8OZ', '').
+			replace(' (STEPHENSON BDAY)', '').
 			replace('SUNSHINE-WITH', 'SUNSHINE WITH').
 			replace('(TOASTEDPECANS)HOPTEST#1', '(TOASTED PECANS)').
 			replace('HELLNATION-HOPTEST#1', 'HELLNATION').
