543a4dabacb133675353894760bdff9a15293ee9
patch-e <p.crager@gmail.com>
2015-10-31 12:58:35 -0400

    Messages constant module

---
+++ frontend/js/shared/ba.shared.Messages.js
@@ -0,0 +1,22 @@
+/*
+ba.shared.Messages.js
+Constant messages used by BeersApp.
+
+Copyright (c) 2015
+
+Patrick Crager
+
+*/
+(function() { 'use strict';
+
+  angular.module('beersApp.shared').constant('Messages', {
+
+    BA_NO_RESULTS: 'No beers match entered filter criteria.',
+    BA_LIST_ERROR: 'Something went wrong when getting the beer list!\n\nRefresh and try again.',
+    BA_UNTAPPD_SEARCH_NOT_FOUND: 'Beer couldn\'t be found on Untappd.\nSorry!',
+    BA_UNTAPPD_SEARCH_ERROR: 'Something went wrong when looking up this beer!\nPlease try again.',
+    BA_UNTAPPD_TOKEN_EXPIRED: 'Your login token has expired, you will now be logged out.',
+
+  });
+
+})();
