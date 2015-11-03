3309a534e61fc8938c7db5739818fde098a5fa24
patch-e <p.crager@gmail.com>
2015-11-03 00:48:17 -0500

    Attribution text moved to a directive

---
+++ frontend/js/directives/ba.directives.Attribution.js
@@ -0,0 +1,26 @@
+/*
+ba.directives.Attribution.js
+Provides formatted attribution text.
+
+Copyright (c) 2015
+
+Patrick Crager
+
+*/
+(function() { 'use strict';
+
+  angular.module('beersApp.directives').directive('attribution', Attribution);
+
+  function Attribution() {
+    var directive = {
+      scope: {
+        isLoading: '@'
+      },
+      restrict: 'EA',
+      template: '<small><em>data provided by <a href="http://alsofhampden.com" target="_blank">alsofhampden.com</a></em></small>'
+    };
+
+    return directive;
+  }
+
+})();
