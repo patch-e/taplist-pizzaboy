b3f9b72290f162079fd7d54751ec811f79a9db6f
patch-e <p.crager@gmail.com>
2015-11-03 00:48:13 -0500

    Navigation fragment moved to a directive

---
+++ frontend/js/directives/ba.directives.Navigation.js
@@ -0,0 +1,24 @@
+/*
+ba.directives.Navigation.js
+Provides the navigation template.
+
+Copyright (c) 2015
+
+Patrick Crager
+
+*/
+(function() { 'use strict';
+
+  angular.module('beersApp.directives').directive('navigation', Navigation);
+
+  function Navigation() {
+    var directive = {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/navigation.html'
+    };
+
+    return directive;
+  }
+
+})();
