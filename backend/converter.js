0ddc2698b9ea46b2a19f6fd6fb0533dd8c228048
patch-e <p.crager@gmail.com>
2016-10-27 20:40:13 -0400

    Refactor

--- backend/converter.js
+++ backend/converter.js
@@ -11,36 +11,38 @@ Patrick Crager
 module.exports = {
 
 	handle: function(beer) {
-		beer.brewery = beer.brewery.
-			replace('PIZZA BOY/ TROEGS/ ABC', 'TROEGS/PIZZA BOY/ABC').
+		beer.brewery = beer.brewery
+			.replace('PIZZA BOY/ TROEGS/ ABC', 'TROEGS/PIZZA BOY/ABC')
 			// only consider the string to the left of a forward slash or ampersand,
 			// common with collab beers that untappd tends to screw up with when searching on
-			replace(/(\/|&).+$/g, '').
+			.replace(/(\/|&).+$/g, '')
 			// additional brewery fixes on a case-by-case basis
-			replace('OSKARBLUES', 'OSKAR BLUES').
-			replace('THE RAVEN', 'BALTIMORE-WASHINGTON BEER WORKS');
+			.replace('OSKARBLUES', 'OSKAR BLUES')
+			.replace('THE RAVEN', 'BALTIMORE-WASHINGTON BEER WORKS')
+			;
 
-		beer.name = beer.name.
+		beer.name = beer.name
 			// remove any bracketed special text in the name, [NITRO], [FIRKIN], [SOUR] etc.
-			replace(/ *\[[^)]*\] */g, '').
+			.replace(/ *\[[^)]*\] */g, '')
 			// remove special hyphen'd text sometimes added to the end as a special notice, ex. BEERNAME -LAST KEG!!
-			replace(/ (-).+$/g, '').
+			.replace(/ (-).+$/g, '')
 			// additional name fixes on a case-by-case basis
-			replace('2013', '').
-			replace('2014', '').
-			replace('2015', '').
-			replace('2016', '').
-			replace('8OZ', '').
-			replace('HONEYCRISP', 'HONEY CRISP').
-			replace('DREAM-ROOT BEER FLOAT', 'DREAM ROOT BEER').
-			replace('JAI-ALAI', 'JAI ALAI').
-			replace('MELONOUS IPA', 'MELONOUS').
-			replace("DALE'S IPA", "DALE'S").
-			replace("GINGER APPLES CIDER", "GINGER APPLES").
-			replace('MADRA ALTA', 'MADRA ALLTA').
-			replace('BLOSSOM HONEY LAGER', 'BLOSSOM HONEY').
-			replace('BRAAAAINS', 'BRAAAIINS').
-			replace('WEED EATER', 'WEEDEATER');
+			.replace('2013', '')
+			.replace('2014', '')
+			.replace('2015', '')
+			.replace('2016', '')
+			.replace('8OZ', '')
+			.replace('HONEYCRISP', 'HONEY CRISP')
+			.replace('DREAM-ROOT BEER FLOAT', 'DREAM ROOT BEER')
+			.replace('JAI-ALAI', 'JAI ALAI')
+			.replace('MELONOUS IPA', 'MELONOUS')
+			.replace("DALE'S IPA", "DALE'S")
+			.replace("GINGER APPLES CIDER", "GINGER APPLES")
+			.replace('MADRA ALTA', 'MADRA ALLTA')
+			.replace('BLOSSOM HONEY LAGER', 'BLOSSOM HONEY')
+			.replace('BRAAAAINS', 'BRAAAIINS')
+			.replace('WEED EATER', 'WEEDEATER')
+			;
 	}
 
 };
