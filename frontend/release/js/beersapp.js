16c9223a5693c9ac5f8f268ddf915f6c69539920
patch-e <p.crager@gmail.com>
2017-03-05 21:30:54 -0500

    Untappd 3.0+ URL schema change

--- frontend/release/js/beersapp.js
+++ frontend/release/js/beersapp.js
@@ -433,7 +433,7 @@ Patrick Crager
 
   angular.module('beersApp.shared').constant('appConfig', {
 
-    BA_UNTAPPD_APP_SCHEME: 'untappd:///?beer=',
+    BA_UNTAPPD_APP_SCHEME: 'untappd://beer/',
     BA_UNTAPPD_URL_SCHEME: 'http://untappd.com/beer/',
     BA_UNTAPPD_CLIENTID: 'B2DA1064B03A353A75E9B035879ECA2CA3E9C3E6',
     BA_UNTAPPD_LOGIN_URL: 'https://untappd.com/oauth/authenticate/?client_id={0}&response_type=code&redirect_url={1}',
